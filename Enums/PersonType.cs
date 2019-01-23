77469b8a8194e900195f3bbb44453f1920e2f847
Nikolay Aryukov <naryukov@pyrus.com>
2019-01-23 12:42:14 +0300

    v2.1.12 added PersonType

---
+++ Enums/PersonType.cs
@@ -0,0 +1,17 @@
+﻿using System.Runtime.Serialization;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace PyrusApiClient
+{
+	[JsonConverter(typeof(StringEnumConverter))]
+	public enum PersonType
+	{
+		[EnumMember(Value = "user")]
+		User,
+		[EnumMember(Value = "bot")]
+		Bot,
+		[EnumMember(Value = "role")]
+		Role
+	}
+}
