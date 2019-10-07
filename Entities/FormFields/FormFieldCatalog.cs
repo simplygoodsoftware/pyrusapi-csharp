b7889f29e1e64b0a8e530fc21af39776695b8389
Dmitry Grigoryev <dgrigoryev@pyrus.com>
2019-10-07 17:35:58 +0300

    Support multiple catalog values

--- Entities/FormFields/FormFieldCatalog.cs
+++ Entities/FormFields/FormFieldCatalog.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using Newtonsoft.Json;
 
 namespace PyrusApiClient
@@ -11,26 +12,34 @@ namespace PyrusApiClient
 
 		public override string ToString()
 		{
-			return Value?.Values == null || Value.Values.Count == 0
+			return Value?.Rows == null || Value.Rows.Length == 0
 				? ""
-				: String.Join(", ", Value.Values);
+				: String.Join("; ", Value.Rows.Select(x => string.Join(", ", x)));
 		}
 	}
 
 	public class Catalog
 	{
+		[Obsolete]
 		[JsonProperty("item_id")]
 		public long ItemId { get; set; }
+
+		[JsonProperty("item_ids")]
+		public long[] ItemIds { get; set; }
 
 		[JsonProperty("headers")]
 		public List<string> Headers { get; set; }
 
+		[Obsolete]
 		[JsonProperty("values")]
 		public List<string> Values { get; set; }
 
+		[JsonProperty("rows")]
+		public string[][] Rows { get; set; }
+
 		public override string ToString()
 		{
-			return ItemId.ToString();
+			return ItemIds != null ? string.Join(", ", ItemIds) : "";
 		}
 	}
 }
