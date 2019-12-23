5e9b89e00773c990dfb380597f0d42fcf12d4287
Nikolay <naryukov@pyrus.com>
2019-12-23 10:52:58 +0300

    fix property visibility

--- Responses/RolesResponse.cs
+++ Responses/RolesResponse.cs
@@ -6,6 +6,6 @@ namespace PyrusApiClient
     public class RolesResponse : ResponseBase
     {
         [JsonProperty(PropertyName = "roles")]
-        List<Role> Roles { get; set; } = new List<Role>();
+        public List<Role> Roles { get; set; } = new List<Role>();
     }
 }
