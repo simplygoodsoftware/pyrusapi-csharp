03b45facc8ecb099d63c1e4158a70ba8424c39e0
Nikolay <naryukov@pyrus.com>
2019-12-23 12:04:26 +0300

    fix roleresponse

--- Responses/RoleResponse.cs
+++ Responses/RoleResponse.cs
@@ -1,10 +1,33 @@
-﻿using Newtonsoft.Json;
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
 
 namespace PyrusApiClient
 {
     public class RoleResponse : ResponseBase
     {
-        [JsonProperty(PropertyName = "role")]
-        public Role Role { get; set; }
+        [JsonProperty("id")]
+        public int? Id { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("external_id")]
+        public string ExternalId { get; set; }
+
+        [JsonProperty("banned")]
+        public bool Banned { get; set; }
+
+        [JsonProperty("member_ids")]
+        public List<int> MemberIds { get; set; } = new List<int>();
+
+        public Role Role =>
+            new Role
+            {
+                Id = Id,
+                Name = Name,
+                ExternalId = ExternalId,
+                Banned = Banned,
+                MemberIds = MemberIds
+            };
     }
 }
