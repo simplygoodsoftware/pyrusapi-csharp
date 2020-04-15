e951f264e55086f87bb94409b386d9d6dcf83a24
Dmitriy Alekseev <dalekseev@pyrus.com>
2020-04-15 16:49:35 +0300

    Moved class to proper place

---
+++ Entities/NewFile.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+
+namespace PyrusApiClient
+{
+    public class NewFile
+	{
+		[JsonProperty("guid")]
+		public string Guid { get; set; }
+
+		[JsonProperty("root_id", DefaultValueHandling = DefaultValueHandling.Ignore)]
+		public int? RootId { get; set; }
+	}
+}
