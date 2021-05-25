89224a5ab60691fdf8470165239855f22ea76828
FyodorLikhachevPyrus <flikhachev@pyrus.com>
2021-05-25 12:45:34 +0300

    replaced spaces with tabs

--- Responses/ChangeMembersResponse.cs
+++ Responses/ChangeMembersResponse.cs
@@ -3,9 +3,9 @@ using System.Collections.Generic;
 
 namespace Pyrus.ApiClient.Responses
 {
-    public class ChangeMembersResponse : ResponseBase
-    {
-        public List<Person> Members { get; set; } = new List<Person>();
-        public List<string> Errors { get; set; } = new List<string>();
-    }
+	public class ChangeMembersResponse : ResponseBase
+	{
+		public List<Person> Members { get; set; } = new List<Person>();
+		public List<string> Errors { get; set; } = new List<string>();
+	}
 }
