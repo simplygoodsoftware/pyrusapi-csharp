55dbb11d5ce121fd79ab900a879fe4e03ce664e6
FyodorLikhachevPyrus <flikhachev@pyrus.com>
2021-06-23 12:40:33 +0300

    added batch role endpoints

--- Requests/UpdateRoleRequest.cs
+++ Requests/UpdateRoleRequest.cs
@@ -5,6 +5,9 @@ namespace PyrusApiClient
 {
 	public class UpdateRoleRequest
 	{
+		[JsonProperty(PropertyName = "id")]
+		public int Id { get; set; }
+
 		[JsonProperty(PropertyName = "name")]
 		public string Name { get; set; }
 
