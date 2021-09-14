b794550f1f9e3c6864ed646e29587900feb9a9a0
Boris Belov <bbelov@pyrus.com>
2021-09-14 11:14:21 +0300

    MultipleCommentsRequest Demo

---
+++ Entities/CommentDescription.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+using PyrusApiClient;
+
+namespace Pyrus.ApiClient.Entities
+{
+    public class CommentDescription
+    {
+        [JsonProperty("task_id")]
+        public int TaskId { get; set; }
+
+        [JsonProperty("previous_note_id")]
+        public long PreviousNoteId { get; set; }
+
+        [JsonProperty("comment")]
+        public TaskCommentRequest TaskChangeRequest { get; set; }
+    }
+}
---
+++ Requests/Builders/TaskBuilders/MultipleCommentsBuilder.cs
@@ -0,0 +1,21 @@
+﻿using Pyrus.ApiClient.Entities;
+using PyrusApiClient;
+using PyrusApiClient.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pyrus.ApiClient.Requests.Builders.TaskBuilders
+{
+    public class MultipleCommentsBuilder
+    {
+        public MultipleCommentsBuilder(List<CommentDescription> comments = null)
+        {
+            Request = new MultipleTasksCommentRequest(comments);
+        }
+
+        public MultipleTasksCommentRequest Request { get; set; }
+    }
+}
