b67937bbf5ff3386ba9cdeaa276ffb508d987124
Alexandr Pyankov <apyankov@pyrus.com>
2022-04-27 19:01:39 +0300

    debug commit

--- ClientExtensions.cs
+++ ClientExtensions.cs
@@ -24,22 +24,22 @@ namespace Pyrus.ApiClient
 				{
 					if (i > 0)
 						await System.Threading.Tasks.Task.Delay(DefaultRetryTimeout);
-					onlyForTest("1", null);
+					onlyForTest?.Invoke("1", null);
 					var res = await action();
 					if (res == null)
 						continue;
-					onlyForTest("2", null);
+					onlyForTest?.Invoke("2", null);
 					if (typeof(TResponse) == typeof(DownloadResponse))
 						return CreateDownloadResponse<TResponse>(res);
-					onlyForTest("3", null);
+					onlyForTest?.Invoke("3", null);
 					if (typeof(TResponse) == typeof(FormRegisterResponse) && res.ToCsv)
 						return new FormRegisterResponse { Csv = res.Message } as TResponse;
-					onlyForTest("4", null);
+					onlyForTest?.Invoke("4", null);
 					try
 					{
-						onlyForTest(res.Message, null);
+						onlyForTest?.Invoke(res.Message, null);
 						result = JsonConvert.DeserializeObject<TResponse>(res.Message, new FormFieldJsonConverter(onlyForTest));
-						onlyForTest(res.Message, JObject.FromObject(result));
+						onlyForTest?.Invoke(res.Message, JObject.FromObject(result));
 					}
 					catch
 					{
