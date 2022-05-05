fdfbca944d7be70ead685e9b3338980043931adf
dkuptsov <dkuptsov@pyrus.com>
2022-05-05 18:04:18 +0300

    semicolon

--- Responses/AnnouncementResponse.cs
+++ Responses/AnnouncementResponse.cs
@@ -5,6 +5,6 @@ namespace PyrusApiClient
 	public class AnnouncementResponse : ResponseBase
 	{
 		[JsonProperty("announcement")]
-		public AnnouncementWithComments Announcement { get; set }
+		public AnnouncementWithComments Announcement { get; set; }
 	}
 }
