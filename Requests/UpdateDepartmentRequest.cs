8e998fc2a68cc239978b6d58bff99f51b24db4a5
Alex Lopatin <minelabory@gmail.com>
2022-05-13 02:43:19 +0300

    renamed head field

--- Requests/UpdateDepartmentRequest.cs
+++ Requests/UpdateDepartmentRequest.cs
@@ -13,8 +13,8 @@ namespace Pyrus.ApiClient.Requests
 		[JsonProperty("parent_id")]
 		public long? ParentId { get; set; }
 
-		[JsonProperty("head_of_department")]
-		public int? HeadOfDepartment { get; set; }
+		[JsonProperty("manager_id")]
+		public int? ManagerId { get; set; }
 
 		[JsonProperty("department_name")]
 		public string DepartmentName { get; set; }
