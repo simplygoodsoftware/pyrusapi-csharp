bbde33f77a4f7204f9a99daab55f451e5b90593e
Alex Lopatin <minelabory@gmail.com>
2022-12-19 17:00:12 +0300

    deleted lists

--- PyrusClient.cs
+++ PyrusClient.cs
@@ -369,9 +369,9 @@ namespace PyrusApiClient
 			return response;
 		}
 
-		public async Task<ListsResponse> GetLists(string accessToken = null, bool withForms = false)
+		public async Task<ListsResponse> GetLists(string accessToken = null, bool withForms = false, bool withDeleted = false)
 		{
-			var url = $"{ClientSettings.Origin}{ListsEndpoint}?with_forms={withForms}";
+			var url = $"{ClientSettings.Origin}{ListsEndpoint}?with_forms={withForms}&with_deleted={withDeleted}";
 			if (accessToken != null)
 				Token = accessToken;
 
