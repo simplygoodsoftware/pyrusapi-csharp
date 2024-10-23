9f99ed475156b29560e27352a8ac9df6d752c60c
Islam Kadyrov <ikadyrov@pyrus.com>
2024-10-23 16:49:35 +0300

    pr fix

--- Enums/SendSmsStatus.cs
+++ Enums/SendSmsStatus.cs
@@ -4,20 +4,20 @@ using Newtonsoft.Json.Converters;
 
 namespace PyrusApiClient
 {
-	[JsonConverter(typeof(StringEnumConverter))]
-	public enum SendSmsStatus
-	{
-		[EnumMember(Value = "sent")]
-		Sent,
+    [JsonConverter(typeof(StringEnumConverter))]
+    public enum SendSmsStatus
+    {
+        [EnumMember(Value = "sent")]
+        Sent,
 
-		[EnumMember(Value = "delivered")]
-		Delivered,
+        [EnumMember(Value = "delivered")]
+        Delivered,
 
-		[EnumMember(Value = "delivery_failed")]
-		DeliveryFailed,
+        [EnumMember(Value = "delivery_failed")]
+        DeliveryFailed,
 
-		[EnumMember(Value = "send_failed")]
-		SendFailed,
+        [EnumMember(Value = "send_failed")]
+        SendFailed,
 
         [EnumMember(Value = "postponed")]
         Postponed,
