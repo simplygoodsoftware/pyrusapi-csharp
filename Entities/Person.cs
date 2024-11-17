33ed16547a9bb268ee35feb142d8b51b548b8891
dkuptsov <dkuptsov@pyrus.com>
2024-11-18 02:39:57 +0300

    add mobile phone

--- Entities/Person.cs
+++ Entities/Person.cs
@@ -4,140 +4,143 @@ using System;
 
 namespace PyrusApiClient
 {
-	public class Person
-	{
-		public Person()
-		{
-		}
+    public class Person
+    {
+        public Person()
+        {
+        }
 
-		public Person(int id)
-		{
-			Id = id;
-		}
+        public Person(int id)
+        {
+            Id = id;
+        }
 
-		public Person(string email)
-		{
-			Email = email;
-		}
+        public Person(string email)
+        {
+            Email = email;
+        }
 
-		[JsonProperty("id")]
-		public int? Id { get; set; }
+        [JsonProperty("id")]
+        public int? Id { get; set; }
 
-		[JsonProperty("first_name")]
-		public string FirstName { get; set; }
+        [JsonProperty("first_name")]
+        public string FirstName { get; set; }
 
-		[JsonProperty("last_name")]
-		public string LastName { get; set; }
+        [JsonProperty("last_name")]
+        public string LastName { get; set; }
 
-		[JsonProperty("native_first_name")]
-		public string NativeFirstName { get; set; }
+        [JsonProperty("native_first_name")]
+        public string NativeFirstName { get; set; }
 
-		[JsonProperty("native_last_name")]
-		public string NativeLastName { get; set; }
+        [JsonProperty("native_last_name")]
+        public string NativeLastName { get; set; }
 
-		[JsonProperty("email")]
-		public string Email { get; set; }
-		
-		[JsonProperty("status")]
-		public string Status { get; set; }
+        [JsonProperty("email")]
+        public string Email { get; set; }
 
-		[JsonProperty("avatar_id")]
-		public int? AvatarId { get; set; }
+        [JsonProperty("status")]
+        public string Status { get; set; }
 
-		[JsonProperty("external_avatar_id")]
-		public int? ExternalAvatarId { get; set; }
+        [JsonProperty("avatar_id")]
+        public int? AvatarId { get; set; }
 
-		[JsonProperty("rights")]
-		public PersonRights? Rights { get; set; }
+        [JsonProperty("external_avatar_id")]
+        public int? ExternalAvatarId { get; set; }
 
-		[JsonProperty("type")]
-		public PersonType Type { get; set; }
+        [JsonProperty("rights")]
+        public PersonRights? Rights { get; set; }
 
-		[JsonProperty("department_id")]
-		public long? DepartmentId { get; set; }
+        [JsonProperty("type")]
+        public PersonType Type { get; set; }
 
-		[JsonProperty("department_name")]
-		public string DepartmentName { get; set; }
+        [JsonProperty("department_id")]
+        public long? DepartmentId { get; set; }
 
-		[JsonProperty("banned")]
-		public bool? Banned { get; set; }
+        [JsonProperty("department_name")]
+        public string DepartmentName { get; set; }
 
-		[JsonProperty("fired")]
-		public bool Fired { get; set; }
+        [JsonProperty("banned")]
+        public bool? Banned { get; set; }
 
-		[JsonProperty("task_receiver")]
-		public int? TaskReceiver { get; set; }
+        [JsonProperty("fired")]
+        public bool Fired { get; set; }
 
-		[JsonProperty("position")]
-		public string Position { get; set; }
+        [JsonProperty("task_receiver")]
+        public int? TaskReceiver { get; set; }
 
-		[Obsolete]
-		[JsonProperty("skype")]
-		public string Skype { get; set; }
+        [JsonProperty("position")]
+        public string Position { get; set; }
 
-		[JsonProperty("messenger")]
-		public Messenger Messenger { get; set; }
+        [Obsolete]
+        [JsonProperty("skype")]
+        public string Skype { get; set; }
 
-		[JsonProperty("phone")]
-		public string Phone { get; set; }
+        [JsonProperty("messenger")]
+        public Messenger Messenger { get; set; }
 
-		[JsonProperty("login_phone")]
-		public string LoginPhone { get; set; }
+        [JsonProperty("phone")]
+        public string Phone { get; set; }
 
-		[JsonProperty("location")]
-		public string Location { get; set; }
+        [JsonProperty("mobile_phone")]
+        public string MobilePhone { get; set; }
 
-		[JsonProperty("personality")]
-		public string Personality { get; set; }
+        [JsonProperty("login_phone")]
+        public string LoginPhone { get; set; }
 
-		[JsonProperty("personnel_number")]
-		public string PersonnelNumber { get; set; }
+        [JsonProperty("location")]
+        public string Location { get; set; }
 
-		[JsonProperty("vacation_days")]
-		public int? VacationDays { get; set; }
+        [JsonProperty("personality")]
+        public string Personality { get; set; }
 
-		[JsonProperty("external_id")]
-		public string ExternalId { get; set; }
+        [JsonProperty("personnel_number")]
+        public string PersonnelNumber { get; set; }
 
-		[JsonProperty("mobile_session_settings")]
-		public SessionLifespan MobileSessionSettings { get; set; }
+        [JsonProperty("vacation_days")]
+        public int? VacationDays { get; set; }
 
-		[JsonProperty("web_session_settings")]
-		public SessionLifespan WebSessionSettings { get; set; }
+        [JsonProperty("external_id")]
+        public string ExternalId { get; set; }
 
-		[JsonProperty("mobile_session_inactive_settings")]
-		public SessionLifespan MobileSessionInactiveSettings { get; set; }
+        [JsonProperty("mobile_session_settings")]
+        public SessionLifespan MobileSessionSettings { get; set; }
 
-		[JsonProperty("web_session_inactive_settings")]
-		public SessionLifespan WebSessionInactiveSettings { get; set; }
+        [JsonProperty("web_session_settings")]
+        public SessionLifespan WebSessionSettings { get; set; }
 
-		[JsonProperty("mobile_session_restriction_settings")]
-		public SessionRestriction MobileSessionRestrictionSettings { get; set; }
+        [JsonProperty("mobile_session_inactive_settings")]
+        public SessionLifespan MobileSessionInactiveSettings { get; set; }
 
-		[JsonProperty("web_session_restriction_settings")]
-		public SessionRestriction WebSessionRestrictionSettings { get; set; }
+        [JsonProperty("web_session_inactive_settings")]
+        public SessionLifespan WebSessionInactiveSettings { get; set; }
 
-		[JsonProperty("cant_view_restricted_form_registry")]
-		public bool? CantViewRestrictedFormRegistry { get; set; }
+        [JsonProperty("mobile_session_restriction_settings")]
+        public SessionRestriction MobileSessionRestrictionSettings { get; set; }
 
-		[JsonProperty("cant_view_form_registry")]
-		public bool? CantViewFormRegistry { get; set; }
+        [JsonProperty("web_session_restriction_settings")]
+        public SessionRestriction WebSessionRestrictionSettings { get; set; }
 
-		[JsonProperty("cant_export_restricted_form_registry")]
-		public bool? CantExportRestrictedFormRegistry { get; set; }
+        [JsonProperty("cant_view_restricted_form_registry")]
+        public bool? CantViewRestrictedFormRegistry { get; set; }
 
-		[JsonProperty("cant_export_form_registry")]
-		public bool? CantExportFormRegistry { get; set; }
+        [JsonProperty("cant_view_form_registry")]
+        public bool? CantViewFormRegistry { get; set; }
 
-		[JsonProperty("cant_add_external_users_to_tasks")]
-		public bool? CantAddExternalUsersToTasks { get; set; }
+        [JsonProperty("cant_export_restricted_form_registry")]
+        public bool? CantExportRestrictedFormRegistry { get; set; }
 
-		[JsonProperty("cant_add_external_users_to_lists")]
-		public bool? CantAddExternalUsersToLists { get; set; }
+        [JsonProperty("cant_export_form_registry")]
+        public bool? CantExportFormRegistry { get; set; }
 
-		[JsonProperty("two_factor_auth")]
-		public bool? TwoFactorAuth { get; set; }
+        [JsonProperty("cant_add_external_users_to_tasks")]
+        public bool? CantAddExternalUsersToTasks { get; set; }
 
-		public override string ToString() => Id?.ToString() ?? Email;
-	}
+        [JsonProperty("cant_add_external_users_to_lists")]
+        public bool? CantAddExternalUsersToLists { get; set; }
+
+        [JsonProperty("two_factor_auth")]
+        public bool? TwoFactorAuth { get; set; }
+
+        public override string ToString() => Id?.ToString() ?? Email;
+    }
 }
--- Responses/MemberResponse.cs
+++ Responses/MemberResponse.cs
@@ -5,157 +5,161 @@ using System;
 
 namespace Pyrus.ApiClient.Responses
 {
-	public class MemberResponse : ResponseBase
-	{
-		[JsonProperty("id")]
-		public int? Id { get; set; }
+    public class MemberResponse : ResponseBase
+    {
+        [JsonProperty("id")]
+        public int? Id { get; set; }
 
-		[JsonProperty("first_name")]
-		public string FirstName { get; set; }
+        [JsonProperty("first_name")]
+        public string FirstName { get; set; }
 
-		[JsonProperty("last_name")]
-		public string LastName { get; set; }
+        [JsonProperty("last_name")]
+        public string LastName { get; set; }
 
-		[JsonProperty("native_first_name")]
-		public string NativeFirstName { get; set; }
+        [JsonProperty("native_first_name")]
+        public string NativeFirstName { get; set; }
 
-		[JsonProperty("native_last_name")]
-		public string NativeLastName { get; set; }
+        [JsonProperty("native_last_name")]
+        public string NativeLastName { get; set; }
 
-		[JsonProperty("email")]
-		public string Email { get; set; }
-		
-		[JsonProperty("status")]
-		public string Status { get; set; }
+        [JsonProperty("email")]
+        public string Email { get; set; }
 
-		[JsonProperty("avatar_id")]
-		public int? AvatarId { get; set; }
+        [JsonProperty("status")]
+        public string Status { get; set; }
 
-		[JsonProperty("external_avatar_id")]
-		public int? ExternalAvatarId { get; set; }
+        [JsonProperty("avatar_id")]
+        public int? AvatarId { get; set; }
 
-		[JsonProperty("rights")]
-		public PersonRights Rights { get; set; }
+        [JsonProperty("external_avatar_id")]
+        public int? ExternalAvatarId { get; set; }
 
-		[JsonProperty("type")]
-		public PersonType Type { get; set; }
+        [JsonProperty("rights")]
+        public PersonRights Rights { get; set; }
 
-		[JsonProperty("department_id")]
-		public int? DepartmentId { get; set; }
+        [JsonProperty("type")]
+        public PersonType Type { get; set; }
 
-		[JsonProperty("department_name")]
-		public string DepartmentName { get; set; }
+        [JsonProperty("department_id")]
+        public int? DepartmentId { get; set; }
 
-		[JsonProperty("banned")]
-		public bool? Banned { get; set; }
+        [JsonProperty("department_name")]
+        public string DepartmentName { get; set; }
 
-		[JsonProperty("position")]
-		public string Position { get; set; }
+        [JsonProperty("banned")]
+        public bool? Banned { get; set; }
 
-		[Obsolete]
-		[JsonProperty("skype")]
-		public string Skype { get; set; }
+        [JsonProperty("position")]
+        public string Position { get; set; }
 
-		[JsonProperty("messenger")]
-		public Messenger Messenger { get; set; }
+        [Obsolete]
+        [JsonProperty("skype")]
+        public string Skype { get; set; }
 
-		[JsonProperty("phone")]
-		public string Phone { get; set; }
+        [JsonProperty("messenger")]
+        public Messenger Messenger { get; set; }
 
-		[JsonProperty("login_phone")]
-		public string LoginPhone { get; set; }
+        [JsonProperty("phone")]
+        public string Phone { get; set; }
 
-		[JsonProperty("location")]
-		public string Location { get; set; }
+        [JsonProperty("mobile_phone")]
+        public string MobilePhone { get; set; }
 
-		[JsonProperty("personality")]
-		public string Personality { get; set; }
+        [JsonProperty("login_phone")]
+        public string LoginPhone { get; set; }
 
-		[JsonProperty("personnel_number")]
-		public string PersonnelNumber { get; set; }
+        [JsonProperty("location")]
+        public string Location { get; set; }
 
-		[JsonProperty("vacation_days")]
-		public int? VacationDays { get; set; }
+        [JsonProperty("personality")]
+        public string Personality { get; set; }
 
-		[JsonProperty("mobile_session_settings")]
-		public SessionLifespan MobileSessionSettings { get; set; }
+        [JsonProperty("personnel_number")]
+        public string PersonnelNumber { get; set; }
 
-		[JsonProperty("web_session_settings")]
-		public SessionLifespan WebSessionSettings { get; set; }
+        [JsonProperty("vacation_days")]
+        public int? VacationDays { get; set; }
 
-		[JsonProperty("mobile_session_inactive_settings")]
-		public SessionLifespan MobileSessionInactiveSettings { get; set; }
+        [JsonProperty("mobile_session_settings")]
+        public SessionLifespan MobileSessionSettings { get; set; }
 
-		[JsonProperty("web_session_inactive_settings")]
-		public SessionLifespan WebSessionInactiveSettings { get; set; }
+        [JsonProperty("web_session_settings")]
+        public SessionLifespan WebSessionSettings { get; set; }
 
-		[JsonProperty("mobile_session_restriction_settings")]
-		public SessionRestriction MobileSessionRestrictionSettings { get; set; }
+        [JsonProperty("mobile_session_inactive_settings")]
+        public SessionLifespan MobileSessionInactiveSettings { get; set; }
 
-		[JsonProperty("web_session_restriction_settings")]
-		public SessionRestriction WebSessionRestrictionSettings { get; set; }
+        [JsonProperty("web_session_inactive_settings")]
+        public SessionLifespan WebSessionInactiveSettings { get; set; }
 
-		[JsonProperty("cant_view_restricted_form_registry")]
-		public bool? CantViewRestrictedFormRegistry { get; set; }
+        [JsonProperty("mobile_session_restriction_settings")]
+        public SessionRestriction MobileSessionRestrictionSettings { get; set; }
 
-		[JsonProperty("cant_view_form_registry")]
-		public bool? CantViewFormRegistry { get; set; }
+        [JsonProperty("web_session_restriction_settings")]
+        public SessionRestriction WebSessionRestrictionSettings { get; set; }
 
-		[JsonProperty("cant_export_restricted_form_registry")]
-		public bool? CantExportRestrictedFormRegistry { get; set; }
+        [JsonProperty("cant_view_restricted_form_registry")]
+        public bool? CantViewRestrictedFormRegistry { get; set; }
 
-		[JsonProperty("cant_export_form_registry")]
-		public bool? CantExportFormRegistry { get; set; }
+        [JsonProperty("cant_view_form_registry")]
+        public bool? CantViewFormRegistry { get; set; }
 
-		[JsonProperty("cant_add_external_users_to_tasks")]
-		public bool? CantAddExternalUsersToTasks { get; set; }
+        [JsonProperty("cant_export_restricted_form_registry")]
+        public bool? CantExportRestrictedFormRegistry { get; set; }
 
-		[JsonProperty("cant_add_external_users_to_lists")]
-		public bool? CantAddExternalUsersToLists { get; set; }
+        [JsonProperty("cant_export_form_registry")]
+        public bool? CantExportFormRegistry { get; set; }
 
-		[JsonProperty("two_factor_auth")]
-		public bool? TwoFactorAuth { get; set; }
+        [JsonProperty("cant_add_external_users_to_tasks")]
+        public bool? CantAddExternalUsersToTasks { get; set; }
 
-		public Person Person =>
-			new Person
-			{ 
-				Id = Id,
-				FirstName = FirstName,
-				LastName = LastName,
-				NativeFirstName = NativeFirstName,
-				NativeLastName = NativeLastName,
-				Email = Email,
-				Type = Type,
-				DepartmentId = DepartmentId,
-				DepartmentName = DepartmentName,
-				Status = Status,
-				AvatarId = AvatarId,
-				ExternalAvatarId = ExternalAvatarId,
-				Banned = Banned,				
-				Position = Position,
+        [JsonProperty("cant_add_external_users_to_lists")]
+        public bool? CantAddExternalUsersToLists { get; set; }
+
+        [JsonProperty("two_factor_auth")]
+        public bool? TwoFactorAuth { get; set; }
+
+        public Person Person =>
+            new Person
+            {
+                Id = Id,
+                FirstName = FirstName,
+                LastName = LastName,
+                NativeFirstName = NativeFirstName,
+                NativeLastName = NativeLastName,
+                Email = Email,
+                Type = Type,
+                DepartmentId = DepartmentId,
+                DepartmentName = DepartmentName,
+                Status = Status,
+                AvatarId = AvatarId,
+                ExternalAvatarId = ExternalAvatarId,
+                Banned = Banned,
+                Position = Position,
 #pragma warning disable CS0612 // Type or member is obsolete
-				Skype = Skype,
+                Skype = Skype,
 #pragma warning restore CS0612 // Type or member is obsolete
-				Messenger = Messenger,
-				Phone = Phone,
-				LoginPhone = LoginPhone,
-				Location = Location,
-				Personality = Personality,
-				PersonnelNumber = PersonnelNumber,
-				VacationDays = VacationDays,
-				MobileSessionSettings = MobileSessionSettings,
-				WebSessionSettings = WebSessionSettings,
-				MobileSessionInactiveSettings = MobileSessionInactiveSettings,
-				WebSessionInactiveSettings = WebSessionInactiveSettings,
-				MobileSessionRestrictionSettings = MobileSessionRestrictionSettings,
-				WebSessionRestrictionSettings = WebSessionRestrictionSettings,
-				CantAddExternalUsersToLists = CantAddExternalUsersToLists,
-				CantAddExternalUsersToTasks = CantAddExternalUsersToTasks,
-				CantExportRestrictedFormRegistry = CantExportRestrictedFormRegistry,
-				CantExportFormRegistry = CantExportFormRegistry,
-				CantViewFormRegistry = CantViewFormRegistry,
-				CantViewRestrictedFormRegistry = CantViewRestrictedFormRegistry,
-				TwoFactorAuth = TwoFactorAuth,
-			};
-	}
+                Messenger = Messenger,
+                Phone = Phone,
+                MobilePhone = MobilePhone,
+                LoginPhone = LoginPhone,
+                Location = Location,
+                Personality = Personality,
+                PersonnelNumber = PersonnelNumber,
+                VacationDays = VacationDays,
+                MobileSessionSettings = MobileSessionSettings,
+                WebSessionSettings = WebSessionSettings,
+                MobileSessionInactiveSettings = MobileSessionInactiveSettings,
+                WebSessionInactiveSettings = WebSessionInactiveSettings,
+                MobileSessionRestrictionSettings = MobileSessionRestrictionSettings,
+                WebSessionRestrictionSettings = WebSessionRestrictionSettings,
+                CantAddExternalUsersToLists = CantAddExternalUsersToLists,
+                CantAddExternalUsersToTasks = CantAddExternalUsersToTasks,
+                CantExportRestrictedFormRegistry = CantExportRestrictedFormRegistry,
+                CantExportFormRegistry = CantExportFormRegistry,
+                CantViewFormRegistry = CantViewFormRegistry,
+                CantViewRestrictedFormRegistry = CantViewRestrictedFormRegistry,
+                TwoFactorAuth = TwoFactorAuth,
+            };
+    }
 }
