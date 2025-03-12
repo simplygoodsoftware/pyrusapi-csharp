2db9f130254ffdce07d0ec56a198c375818f5b24
vkholodilo <vkholodilo@pyrus.com>
2025-03-12 18:55:44 +0300

    Rename include_deleted to include_deleted_items

--- Requests/Builders/CatalogBuilders/GetCatalogRequestBuilder.cs
+++ Requests/Builders/CatalogBuilders/GetCatalogRequestBuilder.cs
@@ -1,4 +1,8 @@
-﻿namespace PyrusApiClient.Builders
+﻿using System;
+using System.Collections.Generic;
+using Pyrus.ApiClient.Entities;
+
+namespace PyrusApiClient.Builders
 {
 	public class GetCatalogRequestBuilder
 	{
@@ -8,12 +12,34 @@
 		public GetCatalogRequestBuilder(int catalogId)
 		{
 			CatalogId = catalogId;
+			_filters = new List<CatalogItemValueFilter>();
 		}
 
 		public GetCatalogRequestBuilder IncludeDeleted()
 		{
+			if (_filters.Count > 0)
+				throw new InvalidOperationException("Filtering deleted items is not supported.");
+
 			IncludeDeletedItems = true;
 			return this;
 		}
+
+		public GetCatalogRequestBuilder AddValueFilter(string columnName, string value)
+		{
+			if (columnName == null)
+				throw new ArgumentNullException(nameof(columnName));
+
+			if (value == null)
+				throw new ArgumentNullException(nameof(value));
+
+			if (IncludeDeletedItems)
+				throw new InvalidOperationException("Filtering deleted items is not suppported.");
+
+			_filters.Add(new CatalogItemValueFilter() { ColumnName = columnName, Value = value});
+			return this;
+		}
+
+		private readonly List<CatalogItemValueFilter> _filters;
+		public IReadOnlyCollection<CatalogItemValueFilter> Filters => _filters;
 	}
 }
--- Requests/GetCatalogRequest.cs
+++ Requests/GetCatalogRequest.cs
@@ -5,8 +5,8 @@ namespace Pyrus.ApiClient.Requests
 {
     public class GetCatalogRequest
     {
-        [JsonProperty(PropertyName = "include_deleted")]
-        public bool IncludeDeleted { get; set; }
+        [JsonProperty(PropertyName = "include_deleted_items")]
+        public bool IncludeDeletedItems { get; set; }
 
         [JsonProperty(PropertyName = "filters")]
         public CatalogItemValueFilter[] Filters { get; set; }
