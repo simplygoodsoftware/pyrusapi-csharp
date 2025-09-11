57a598cc7b8122f145f8cc1dc13b3f80cf616c69
dkuptsov <dkuptsov@pyrus.com>
2025-09-12 01:44:51 +0300

    add new filter

---
+++ Requests/FormFilters/ExistsFilter.cs
@@ -0,0 +1,23 @@
+﻿namespace PyrusApiClient
+{
+	public class ExistsFilter : FormFilter
+	{
+		public ExistsFilter(int fieldId)
+			: base(OperatorId.Exists)
+		{
+			FieldId = fieldId;
+		}
+
+		public ExistsFilter(string fieldName, FormResponse form)
+			: base(OperatorId.Exists)
+		{
+			FieldId = GetFieldIdByName(fieldName, form);
+		}
+
+		public ExistsFilter(string fieldName)
+			:base(OperatorId.Exists)
+		{
+			FieldName = fieldName;
+		}
+	}
+}
