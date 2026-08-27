2dee6e13775099c984aa118d8cd885ad1ab697fd
Евгений Вилков <evilkov@pyrus.com>
2026-08-27 16:12:33 +0300

    Tabs + version

--- Requests/Builders/TaskCommentBuilders/FormTaskCommentBuilder.cs
+++ Requests/Builders/TaskCommentBuilders/FormTaskCommentBuilder.cs
@@ -168,17 +168,17 @@ namespace PyrusApiClient.Builders
 			return this;
 		}
 
-        public FormTaskCommentBuilder SendToCustomChannel(int integrationId)
-        {
-            Comment.Channel = new Channel
-            {
-                Type = ChannelType.Custom,
+		public FormTaskCommentBuilder SendToCustomChannel(int integrationId)
+		{
+			Comment.Channel = new Channel
+			{
+				Type = ChannelType.Custom,
 				IntegrationId = integrationId
-            };
-            return this;
-        }
+			};
+			return this;
+		}
 
-        public FormTaskCommentBuilder SendToSmsChannel(string phone)
+		public FormTaskCommentBuilder SendToSmsChannel(string phone)
 		{
 			Comment.Channel = new Channel
 			{
@@ -198,15 +198,15 @@ namespace PyrusApiClient.Builders
 			return this;
 		}
 
-        public FormTaskCommentBuilder SendToMaxMessengerChannel()
-        {
-            Comment.Channel = new Channel
-            {
-                Type = ChannelType.MaxMessenger
-            };
+		public FormTaskCommentBuilder SendToMaxMessengerChannel()
+		{
+			Comment.Channel = new Channel
+			{
+				Type = ChannelType.MaxMessenger
+			};
 
-            return this;
-        }
+			return this;
+		}
 
 		public FormTaskCommentBuilder EditExistingComment(long commentId)
 		{
